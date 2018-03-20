a1ebae48f5095b5f7dcc12c907c53dba2a3cf304
Daniel <danielrferriss@gmail.com>
2018-03-20 01:10:50 -0500

    successflly converts image to ascii

--- image-convert.py
+++ image-convert.py
@@ -7,7 +7,7 @@ import numpy as np
 # file = input("Please enter file name: ")
 # pixels = input("Please enter pixels: ")
 file = 'mountain.jpg'
-pixellation = 50
+pixellation = 10
 
 image = io.imread(os.path.join('mountain.jpg'))
 
@@ -22,7 +22,8 @@ height = len(gray_image)
 
 nlength = int(length / pixellation)
 nheight = int(height / pixellation)
-nimage = np.ndarray(shape=(nheight, nlength), dtype = float)
+nimage_values = np.ndarray(shape=(nheight, nlength), dtype = float)
+nimage = np.ndarray(shape=(nheight, nlength), dtype = str)
 
 for x in range(nlength):
 	for y in range(nheight):
@@ -32,10 +33,35 @@ for x in range(nlength):
 			for yy in range((y * pixellation), ((y + 1) * pixellation)):
 				count = count + 1
 				total = total + gray_image[yy][xx]
-		nimage[y][x] = total / count
+		nimage_values[y][x] = total / count
+
+def to_ascii(x):
+	if x > .9:
+		return '█'
+	if x > .75:
+		return '▓'
+	if x > .5:
+		return '▒'
+	if x > .25:
+		return '░'
+	return ' '
+
+for y in range(nheight):
+	line = ''
+	for x in range(nlength):
+		line = line + to_ascii(nimage_values[y][x])
+	print(line)
 
 
-ax1.imshow(gray_image, cmap="gray")
-ax2.imshow(nimage, cmap="gray")
 
-plt.show()
+
+
+
+
+
+
+
+# ax1.imshow(gray_image, cmap="gray")
+# ax2.imshow(nimage, cmap="gray")
+
+# plt.show()
