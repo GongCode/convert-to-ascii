8f3ee72db461e5c0da2ddf9b6fd4ca79d3ddce29
Daniel <danielrferriss@gmail.com>
2018-03-20 20:51:42 -0500

    change y pixellation to be divided by 2

--- image-convert.py
+++ image-convert.py
@@ -4,36 +4,31 @@ from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
 import numpy as np
 
-# file = input("Please enter file name: ")
-# pixels = input("Please enter pixels: ")
-file = 'mountain.jpg'
-pixellation = 10
+#file is name of file you want to convert
+#pixellation is square width of how many pixels of original should be in one pixel of output
+def img2ascii(image, pixellation):
+	#defining array that stores the value of each pixel in the new image array
+	ascii_length = int(len(gray_image[1]) / pixellation)
+	ascii_height = int(len(gray_image) / pixellation / 2)
+	ascii_image_values = np.ndarray(shape=(ascii_height, ascii_length), dtype = float)
+	#x and y are the position in the new ascii image
+	for x in range(ascii_length):
+		for y in range(2 * ascii_height):
+			#find the average value of the pixe in the grayscale original image
+			count = 0
+			total = 0
+			#gray_x and gray_y are the x and y position in the original grayscale image
+			for gray_x in range((x * pixellation), ((x + 1) * pixellation)):
+				for gray_y in range((y * pixellation), ((y + 1) * pixellation)):
+					count = count + 1
+					total = total + gray_image[gray_y][gray_x]
+			ascii_image_values[int(y / 2)][x] = total / count
 
-image = io.imread(os.path.join('mountain.jpg'))
-
-gray_image = rgb2gray(image)
-
-fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(8, 4),
-                               sharex=False, sharey=False)
-
-
-length = len(gray_image[1])
-height = len(gray_image)
-
-nlength = int(length / pixellation)
-nheight = int(height / pixellation)
-nimage_values = np.ndarray(shape=(nheight, nlength), dtype = float)
-nimage = np.ndarray(shape=(nheight, nlength), dtype = str)
-
-for x in range(nlength):
-	for y in range(nheight):
-		count = 0
-		total = 0
-		for xx in range((x * pixellation), ((x + 1) * pixellation)):
-			for yy in range((y * pixellation), ((y + 1) * pixellation)):
-				count = count + 1
-				total = total + gray_image[yy][xx]
-		nimage_values[y][x] = total / count
+	for y in range(ascii_height):
+		line = ''
+		for x in range(ascii_length):
+			line = line + to_ascii(ascii_image_values[y][x])
+		print(line)
 
 def to_ascii(x):
 	if x > .9:
@@ -46,22 +41,20 @@ def to_ascii(x):
 		return '░'
 	return ' '
 
-for y in range(nheight):
-	line = ''
-	for x in range(nlength):
-		line = line + to_ascii(nimage_values[y][x])
-	print(line)
+def print_ascii(ascii_image):
+	for x in range(len(ascii_image)):
+		line = ''
+		for y in range(len(ascii_image[0])):
+			line = line + ascii_image[x][y]
+		print(line)
 
 
 
+#name of file you want to convert
+file = 'mountain.jpg'
+#square width of how many pixels of original should be in one pixel of output
+pixellation = 10
+#opening then converting image to grayscale
+gray_image = rgb2gray(io.imread(os.path.join('mountain.jpg')))
 
-
-
-
-
-
-
-# ax1.imshow(gray_image, cmap="gray")
-# ax2.imshow(nimage, cmap="gray")
-
-# plt.show()
+img2ascii(gray_image, pixellation)
