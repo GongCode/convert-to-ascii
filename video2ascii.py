46d56890322dea58282c284d706db9a1cc0acc6a
Daniel <danielrferriss@gmail.com>
2018-03-23 23:25:11 -0500

    added default parameters

--- video2ascii.py
+++ video2ascii.py
@@ -1,31 +1,30 @@
 import cv2
 import time
 from tqdm import tqdm
-from imageconvert import img2ascii, print_ascii
+from image2ascii import img2ascii, print_ascii
 
-def video2ascii(video, pixellation):
+#video is video to convert
+#pixellation is square width of how many pixels of original should be in one pixel of output
+def video2ascii(video, pixellation=10):
 	vidcap = cv2.VideoCapture(video)
 	vidlength = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+	#used in printing
+	wait = 1.0/vidcap.get(cv2.CAP_PROP_FPS)
 
 	success,image = vidcap.read()
 	img_arr = [] 
 
+	#tqdm makes a status bar
 	for count in tqdm(range(vidlength - 1)):
 		success,image = vidcap.read()
-		if cv2.waitKey(10) == 27:
-			break
 		img_arr.append(img2ascii(image, pixellation))
-	return img_arr
+	#return tuple of array of frames and wait time between frames to print
+	return (img_arr, wait)
 
-
-
-pixellation = 10
-video = 'fortheculture.mp4'
-ascii_video = video2ascii(video, pixellation)
-
-while True:
-	for x in range(len(ascii_video)):
-		for y in range(30):
-			print(flush=True)
-		print_ascii(ascii_video[x])
-		time.sleep(.04)
+#video is array of images. wait is time to wait between printing each image
+#default wait will make the video 24 fps
+def print_video(video, wait=.04166):
+	for x in range(len(video)):
+		print_ascii(video[x])
+		time.sleep(wait)
